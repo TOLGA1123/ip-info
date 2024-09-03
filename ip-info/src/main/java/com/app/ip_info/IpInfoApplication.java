56a48107b6844a8eab465b600f7726d35e1f9dfa
Tolgahan Arslan <90002820+TOLGA1123@users.noreply.github.com>
2024-09-03 10:30:10 +0300

    Initial commit

---
+++ ip-info/src/main/java/com/app/ip_info/IpInfoApplication.java
@@ -0,0 +1,13 @@
+package com.app.ip_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpInfoApplication.class, args);
+	}
+
+}
