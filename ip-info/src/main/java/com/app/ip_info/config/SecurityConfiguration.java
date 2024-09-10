fb9a7212fde8fe2a972c47f6ba2eeabf1bb8f20d
Tolgahan Arslan <90002820+TOLGA1123@users.noreply.github.com>
2024-09-10 09:59:24 +0300

    docker files backend

--- ip-info/src/main/java/com/app/ip_info/config/SecurityConfiguration.java
+++ ip-info/src/main/java/com/app/ip_info/config/SecurityConfiguration.java
@@ -49,7 +49,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));        //3000 on local, and docker
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
